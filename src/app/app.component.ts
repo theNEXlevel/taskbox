5a7b9acce399483bc5cfc58546bae24010086db1
Justin McLellan <jmclellan@crexi.com>
2022-10-12 11:34:35 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: ` <app-inbox-screen></app-inbox-screen> `,
+})
+export class AppComponent {
+  title = 'taskbox';
+}
---
+++ src/app/components/task-list.component.ts
@@ -0,0 +1,37 @@
+
+import { Component } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { ArchiveTask, PinTask } from '../state/task.state';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-task-list',
+  template: `
+    <app-pure-task-list
+      [tasks]="tasks$ | async"
+      (onArchiveTask)="archiveTask($event)"
+      (onPinTask)="pinTask($event)"
+    ></app-pure-task-list>
+  `,
+})
+export class TaskListComponent {
+  tasks$?: Observable<any>;
+
+  constructor(private store: Store) {
+     this.tasks$ = store.select((state) => state.taskbox.tasks);
+  }
+
+  /**
+   * Component method to trigger the archiveTask event
+   */
+  archiveTask(id: string) {
+    this.store.dispatch(new ArchiveTask(id));
+  }
+
+  /**
+   * Component method to trigger the pinTask event
+   */
+  pinTask(id: string) {
+    this.store.dispatch(new PinTask(id));
+  }
+}
